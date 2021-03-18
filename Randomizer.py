23e317fb39b9e2c70609db9b0357405ec9cdcb2f
NaufalAmr <80613428+NaufalAmr@users.noreply.github.com>
2021-03-18 19:51:48 +0700

    Add files via upload

---
+++ Randomizer.py
@@ -0,0 +1,73 @@
+import random as r
+
+def rollADice():
+    dice = int(input("Jumlah dadu : "))
+    while True:
+        result = r.randint(1*dice, 6*dice)
+        print(result)
+        userReq = input("Lempar dadu lagi? [y/n] : ")
+        if userReq == "n":
+            break
+    
+
+def flipCoin():
+    while True:
+        print(r.choice(["kepala", "ekor"]), "!!")
+        userReq = input("Lempar koin lagi? [y/n] : ")
+        if userReq == "n":
+            break
+
+def randomNumber():
+    numRange = input("masukkan range angka (misal 1-10) : ")
+    data = numRange.split("-")
+    while True:
+        print(r.randint(int(data[0]), int(data[1])))
+        userCmnd = input("Acak lagi ? [y/n] : ")
+        if userCmnd == "n":
+            break
+
+def randomName():
+    count = int(input("Jumlah nama yg akan diacak : "))
+    nameData = []
+    for _ in range(0,count):
+        name = input("Masukkan nama : ")
+        nameData.append(name)
+    print("""
+1.Ambil satu nama
+2.Buat urutan nama acak
+========================""")
+    userReq = input("Masukkan pilihan : ")
+    
+    while True:
+        if userReq == "1":
+            print(r.choice(nameData))
+        elif userReq == "2":
+            r.shuffle(nameData)
+            for key,val in enumerate(nameData, start=1):
+                print(f"{key}.{val}")
+        userCmnd = input("Acak lagi ? [y/n] : ")
+        if userCmnd == "n":
+            break
+
+def main():
+    while True:
+        print("""
+==================
+    RANDOMIZER
+==================
+1. Lempar dadu
+2. Lempar koin
+3. Acak angka
+4. Acak nama""")
+        userRequest = input("Masukkan nomor pillihan : ")
+        if userRequest == "1":
+            rollADice()
+        elif userRequest == "2":
+            flipCoin()
+        elif userRequest == "3":
+            randomNumber()
+        elif userRequest == "4":
+            randomName()
+
+if __name__ == "__main__":
+    main()
